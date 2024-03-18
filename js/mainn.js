a0ed3b5a1fe9c7a433db8c9bf4ef658056be56dd
hekichien <chienlx1005@gmail.com>
2024-03-18 15:57:07 +0700

    update sticky banner

--- js/mainn.js
+++ js/mainn.js
@@ -153,3 +153,20 @@ $(window).resize(function () {
 });
 
 $(window).resize();
+
+
+
+class stickyBanner extends HTMLElement{
+  constructor(){
+    super();
+    this.btn =this.querySelector('button.close');
+
+    this.btn.addEventListener('click',()=>{
+      this.querySelector('.banner-wrap').setAttribute('hide','')
+      setTimeout(() => {
+        this.setAttribute('hide','');
+      }, 500);
+    })
+  }
+}
+customElements.define('sticky-banner',stickyBanner)
